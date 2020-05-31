150f36ca2dbfc620f6c16fa89684a518805f4583
925213723 <925213723@qq.com>
2020-06-01 00:16:28 +0800

    乐优 customers

---
+++ src/main/java/com/leyou/service/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.leyou.service.controller;
+
+import com.leyou.service.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("customer/user")
+public class UserController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping
+    @ResponseBody
+    public User queryUserId(@RequestParam("id") Long id){
+        // 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+        // 因为只有一个Service-provider。所以获取第一个实例
+        ServiceInstance instance = instances.get(0);
+        // 获取ip和端口信息，拼接成服务地址
+        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id;
+        User user = this.restTemplate.getForObject(baseUrl, User.class);
+        return user;
+    }
+}
